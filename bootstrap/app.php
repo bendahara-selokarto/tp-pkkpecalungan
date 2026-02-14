8bd572b89b1f4a7bf3b71ea2f5d8cdf116126e04
bendahara-selokarto <bendaharadesaselokarto@gmail.com>
2026-02-14 23:04:21 +0700

    fix(auth): render friendly 403 page for role authorization failures

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 
@@ -19,6 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (UnauthorizedException $exception, $request) {
+            return response()->view('errors.403', [
+                'message' => 'Anda tidak memiliki akses ke halaman ini.',
+            ], 403);
+        });
     })
     ->create();
